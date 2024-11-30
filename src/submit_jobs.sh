fd31f58c7c0546e4b74e0a01d3251deaadc77c70
Jan Bronec <xelfxendr@gmail.com>
2024-11-30 18:49:59 +0100

    small fix

--- src/submit_jobs.sh
+++ src/submit_jobs.sh
@@ -3,7 +3,7 @@
 # try various values for beta and learning rate
 for beta in 0.1 0.5 0.1 0.15
 do
-    for lr in '1e-3' '1e-4' '1e-5'
+    for lr in 1e-3 1e-4 1e-5
     do
         # submit the job
         qsub -q gpu -v BETA=$beta,LR=$lr unlearn.sh

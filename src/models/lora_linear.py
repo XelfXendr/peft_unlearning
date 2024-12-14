a449555c3b1a49112abd607da570578d3ec86f1d
Jan Bronec <xelfxendr@gmail.com>
2024-12-14 19:36:25 +0100

    File structuring and formatting

---
+++ src/models/lora_linear.py
@@ -0,0 +1,20 @@
+import torch
+
+class LoRALinear(torch.nn.Module):
+    def __init__(self, original: torch.nn.Linear, rank: int):
+        super().__init__()
+
+        self.original = original
+        self.A = torch.nn.Linear(original.in_features, rank, bias=False)
+        self.B = torch.nn.Linear(rank, original.out_features, bias=False)
+
+        self._only_backbone = False
+
+        torch.nn.init.xavier_normal_(self.A.weight)
+        torch.nn.init.zeros_(self.B.weight)
+
+    def forward(self, x):
+        if self._only_backbone:
+            return self.original(x)
+        else:
+            return self.original(x) + self.B(self.A(x))
---
+++ src/models/lora_model.py
@@ -0,0 +1,41 @@
+import torch
+from transformers import AutoModelForCausalLM
+from models import LoRALinear
+
+
+class LoRAModel(torch.nn.Module):
+    def __init__(
+        self,
+        model: AutoModelForCausalLM,
+        rank: int = 2,
+        to_adapt: list[str] = ["q_proj", "v_proj"],
+    ):
+        super().__init__()
+        self._llm: AutoModelForCausalLM = model
+        self._loras: list[LoRALinear] = []
+
+        # freeze all parameters of the llm
+        for param in self._llm.parameters():
+            param.requires_grad = False
+
+        # change projections selected projections to LoRALinear
+        modules = list(self._llm.named_modules())
+        for name, module in modules:
+            if isinstance(module, torch.nn.Linear) and any(
+                proj in name for proj in to_adapt
+            ):
+                parent_name, attr_name = name.rsplit(".", 1)
+                parent_module = self._llm.get_submodule(parent_name)
+
+                lora = LoRALinear(module, rank)
+                self._loras.append(lora)
+                lora.A.weight.requires_grad = True
+                lora.B.weight.requires_grad = True
+                setattr(parent_module, attr_name, lora)
+
+    def only_backbone(self, only_backbone: bool):
+        for lora in self._loras:
+            lora._only_backbone = only_backbone
+
+    def forward(self, x, **xs):
+        return self._llm(x, **xs)

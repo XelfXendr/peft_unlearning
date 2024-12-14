de5e98217a0fbb4bacc45a3d36a120480571be66
Jan Bronec <bronecja@skirit.metacentrum.cz>
2024-12-14 13:57:22 +0100

    submission script changes

--- src/submit_jobs.sh
+++ src/submit_jobs.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # try various values for beta and learning rate
-for beta in 0.1 0.5 0.1 0.15
+for beta in 1.0 1.1
 do
-    for lr in 1e-3 1e-4 1e-5
+    for lr in 1e-4 1e-5
     do
         # submit the job
         qsub -q gpu -v BETA=$beta,LR=$lr unlearn.sh

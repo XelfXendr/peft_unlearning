446294fd554c961674e6398a90e00af0584fe6ff
Xelf <xelfxendr@gmail.com>
2025-01-02 13:15:48 -0600

    larger epochs

--- src/submit_7B_loras.sh
+++ src/submit_7B_loras.sh
@@ -6,6 +6,6 @@ do
     for rank in 1 2 5 10
     do
         # submit the job
-        qsub -q gpu -v MODEL=7B,EP=20,BETA=0.5,LR=1e-4,NPO=1.,RT=1.,KL=0.5,MERGE=$merge,RANK=$rank unlearn-7B.sh
+        qsub -q gpu -v MODEL=7B,EP=30,BETA=0.5,LR=1e-4,NPO=1.,RT=1.,KL=0.5,MERGE=$merge,RANK=$rank unlearn-7B.sh
     done
 done

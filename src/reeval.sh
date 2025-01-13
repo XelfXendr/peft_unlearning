8a06f03297dda278ae88363d72aa59a3abdfd7a9
Jan Bronec <bronecja@skirit.metacentrum.cz>
2025-01-13 17:49:08 +0100

    reeval.sh fixes

--- src/reeval.sh
+++ src/reeval.sh
@@ -3,6 +3,6 @@
 for f in $(find "/storage/brno2/home/bronecja/llm_thesis_logs" -maxdepth 1)
 do
    if [ -d $f'/model' ]; then
-      qsub -q gpu -v RUNLOGDIR=$f just_eval.sh
+      qsub -q gpu -v RUNLOGDIR="'$f'" just_eval.sh
    fi
 done

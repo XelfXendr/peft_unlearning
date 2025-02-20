683f57ef7e5b43bf83c17a8465b5f441f4b2ae33
Jan Bronec <xelfxendr@gmail.com>
2025-02-20 22:44:50 +0100

    small fix

--- jobs/spawn_unlearn_jobs.sh
+++ jobs/spawn_unlearn_jobs.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 for i in 0 1 2 3 4
 do
-    SEED=$(date +%s%N)
-    qsub -q gpu -v MODEL=7B,EP=20,LR=1e-4,BETA=0.5,NPO=1.,RT=1.,KL=0.5,RANK=5,SEED=$SEED,SAVE=5 unlearn_eval.sh
-    qsub -q gpu -v MODEL=7B,EP=20,LR=1e-4,BETA=0.5,NPO=1.,RT=1.,KL=0,RANK=5,SEED=$SEED,SAVE=5 unlearn_eval.sh
-    qsub -q gpu -v MODEL=7B,EP=20,LR=1e-4,BETA=0.5,NPO=1.,RT=1.,KL=1.,RANK=5,SEED=$SEED,SAVE=5 unlearn_eval.sh
-    qsub -q gpu -v MODEL=7B,EP=20,LR=1e-4,BETA=0.5,NPO=1.,RT=0.,KL=1.,RANK=5,SEED=$SEED,SAVE=5 unlearn_eval.sh
+    SEED=$(date +%N)
+    qsub -q gpu -v MODEL=7B,EP=20,LR=1e-4,BETA=0.5,NPO=1.,RT=1.,KL=0.5,RANK=5,SEED=$SEED,SAVE=5 unlearn_job.sh
+    qsub -q gpu -v MODEL=7B,EP=20,LR=1e-4,BETA=0.5,NPO=1.,RT=1.,KL=0,RANK=5,SEED=$SEED,SAVE=5 unlearn_job.sh
+    qsub -q gpu -v MODEL=7B,EP=20,LR=1e-4,BETA=0.5,NPO=1.,RT=1.,KL=1.,RANK=5,SEED=$SEED,SAVE=5 unlearn_job.sh
+    qsub -q gpu -v MODEL=7B,EP=20,LR=1e-4,BETA=0.5,NPO=1.,RT=0.,KL=1.,RANK=5,SEED=$SEED,SAVE=5 unlearn_job.sh
 done
